488637dc37de556c0acaadc4d4249e7604d20790
erditona <erditonaushaadam@gmail.com>
2024-06-27 15:01:07 +0700

    add controller

---
+++ controller/chat.go
@@ -0,0 +1,59 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Barokah-AI/BackEnd/helper"
+	"github.com/Barokah-AI/BackEnd/model"
+	"github.com/go-resty/resty/v2"
+)
+
+func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
+	var chat model.AIRequest
+
+	err := json.NewDecoder(req.Body).Decode(&chat)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
+
+	if chat.Promt == "" {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		return
+	}
+
+	client := resty.New()
+
+    // Hugging Face API URL dan token
+    apiUrl := "https://api-inference.huggingface.co/models/dimasardnt/barokah-model"
+    apiToken := "Bearer " + "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"
+
+	response, err := client.R().
+            SetHeader("Authorization", apiToken).
+            SetHeader("Content-Type", "application/json").
+            SetBody(`{"inputs": "`+chat.Promt+`"}`).
+            Post(apiUrl)
+
+        if err != nil {
+            log.Fatalf("Error making request: %v", err)
+        }
+
+	var data []map[string]string
+
+	err = json.Unmarshal([]byte(response.String()), &data)
+	if err != nil {
+        fmt.Println("Response:", response.String())
+        fmt.Println("token", tokenmodel)
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+
+	if len(data) > 0 {
+		helper.WriteJSON(respw, http.StatusOK, data[0])
+	} else {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : response")
+	}
+}
