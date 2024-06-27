8859239124d41375cc86f630822fdb99f6d8b491
dimasardnt6 <dimasardnt6@gmail.com>
2024-06-27 18:10:52 +0700

    update chat.go

--- controller/chat.go
+++ controller/chat.go
@@ -5,13 +5,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
 
+	"github.com/Barokah-AI/BackEnd/config"
 	"github.com/Barokah-AI/BackEnd/helper"
 	"github.com/Barokah-AI/BackEnd/model"
 	"github.com/go-resty/resty/v2"
 )
 
-func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
+// Struct untuk hasil prediksi
+type Prediction struct {
+	Label string  `json:"label"`
+	Score float64 `json:"score"`
+}
+
+func Chat(respw http.ResponseWriter, req *http.Request) {
 	var chat model.AIRequest
 
 	err := json.NewDecoder(req.Body).Decode(&chat)
@@ -27,32 +35,38 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 
 	client := resty.New()
 
-    // Hugging Face API URL dan token
-    apiUrl := "anu"
-    apiToken := "Bearer " + "ini"
+	// Hugging Face API URL dan token
+	apiUrl := "https://api-inference.huggingface.co/models/dimasardnt/barokah-model"
+	apiToken := "Bearer " + config.GetEnv("TOKENMODEL")
 
 	response, err := client.R().
-            SetHeader("Authorization", apiToken).
-            SetHeader("Content-Type", "application/json").
-            SetBody(`{"inputs": "`+chat.Promt+`"}`).
-            Post(apiUrl)
+		SetHeader("Authorization", apiToken).
+		SetHeader("Content-Type", "application/json").
+		SetBody(`{"inputs": "` + chat.Promt + `"}`).
+		Post(apiUrl)
 
-        if err != nil {
-            log.Fatalf("Error making request: %v", err)
-        }
-
-	var data []map[string]string
-
-	err = json.Unmarshal([]byte(response.String()), &data)
 	if err != nil {
-        fmt.Println("Response:", response.String())
-        fmt.Println("token", tokenmodel)
+		log.Fatalf("Error making request: %v", err)
+	}
+
+	var predictions [][]Prediction
+
+	err = json.Unmarshal(response.Body(), &predictions)
+	if err != nil {
+		fmt.Println("Response:", response.String())
 		fmt.Println("Error decoding JSON:", err)
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : response")
 		return
 	}
 
-	if len(data) > 0 {
-		helper.WriteJSON(respw, http.StatusOK, data[0])
+	if len(predictions) > 0 && len(predictions[0]) > 0 {
+		// Sort predictions by score in descending order
+		sort.Slice(predictions[0], func(i, j int) bool {
+			return predictions[0][i].Score > predictions[0][j].Score
+		})
+
+		// Send the top prediction
+		helper.WriteJSON(respw, http.StatusOK, predictions[0][0])
 	} else {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : response")
 	}
