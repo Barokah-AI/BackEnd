84fc97b08255076697ac90b23d9a1db6761cf56d
erditona <erditonaushaadam@gmail.com>
2024-07-05 21:56:37 +0700

    update type.go

--- model/type.go
+++ model/type.go
@@ -4,23 +4,9 @@ import (
 	"time"
 )
 
-// type ChatRequest struct {
-// 	Message string `json:"message"`
-// }
-
-// type ChatResponse struct {
-// 	Response string `json:"response"`
-// }
-
-// type HuggingFaceRequest struct {
-// 	Inputs string `json:"inputs"`
-// }
-
-// type HuggingFaceResponse struct {
-// 	GeneratedText string `json:"generated_text"`
-// }
 
 type AIRequest struct {
-	Promt   	    string             `bson:"promt,omitempty" json:"promt,omitempty"`
+	Prompt   	    string             `bson:"promt,omitempty" json:"promt,omitempty"`
+	AIResp          string             `bson:"airesp,omitempty" json:"airesp,omitempty"`
 	CreatedAt       time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"`
 }
