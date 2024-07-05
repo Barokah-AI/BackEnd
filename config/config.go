0a8a8c69cef32e38a22d529472de5ca10c6320e2
erditona <erditonaushaadam@gmail.com>
2024-07-05 21:36:44 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,10 +1,29 @@
 package config
 
 import (
+	"log"
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
-func GetEnv(envName string) string {
-	envFile, _ := godotenv.Read("../.env")
-	return envFile[envName]
+// LoadEnv loads the environment variables from a .env file
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+// GetEnv gets the environment variable by name
+func GetEnv(envName string, defaultValue string) string {
+	value, exists := os.LookupEnv(envName)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+func init() {
+	LoadEnv()
 }
