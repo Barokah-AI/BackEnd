1d05b4197209cd0968815e908fafb5ca5a7ace4e
AdeCandra12 <ade.can321@gmail.com>
2024-07-07 15:02:53 +0700

    update cors.go

--- config/cors.go
+++ config/cors.go
@@ -8,6 +8,9 @@ import (
 var Origins = []string{
 	"http://localhost:8080",
 	"http://localhost:5173",
+	"http://127.0.0.1:5500",
+	"http://127.0.0.1:5501",
+	"http://127.0.0.1:5503",
 }
 
 // Fungsi untuk memeriksa apakah origin diizinkan
