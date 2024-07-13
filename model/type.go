aaff18c1701166c9b5d8ab73cc67513b9173b72d
erditona <erditonaushaadam@gmail.com>
2024-07-13 15:24:54 +0700

    update type.go

--- model/type.go
+++ model/type.go
@@ -4,9 +4,29 @@ import (
 	"time"
 )
 
-
+// Struct untuk membaca request dari user
 type AIRequest struct {
 	Prompt   	    string             `bson:"prompt,omitempty" json:"prompt,omitempty"`
 	AIResp          string             `bson:"airesp,omitempty" json:"airesp,omitempty"`
 	CreatedAt       time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"`
+}
+
+// Struct untuk membaca request dari Hugging Face API
+type HFRequest struct {
+    Inputs string `json:"inputs"`
+}
+
+type HFResponse struct {
+    Label string  `json:"label"`
+    Score float64 `json:"score"`
+}
+
+// Struct untuk membaca tokenizer_config.json
+type TokenizerConfig struct {
+	DoLowerCase   bool   `json:"do_lower_case"`
+	ClsToken      string `json:"cls_token"`
+	PadToken      string `json:"pad_token"`
+	SepToken      string `json:"sep_token"`
+	MaskToken     string `json:"mask_token"`
+	UnkToken      string `json:"unk_token"`
 }
