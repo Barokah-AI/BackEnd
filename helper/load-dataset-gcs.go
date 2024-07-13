3451f75432d7a820a8bd1ade6f088ee366a5db6a
erditona <erditonaushaadam@gmail.com>
2024-07-13 18:25:02 +0700

    update load-dataset-gcs

--- helper/load-dataset-gcs.go
+++ helper/load-dataset-gcs.go
@@ -1,32 +1,21 @@
 package helper
 
 import (
-	"context"
+	"bytes"
 	"encoding/csv"
 	"fmt"
 	"log"
 	"strconv"
-
-	"cloud.google.com/go/storage"
 )
 
 // LoadDataset loads the dataset from GCS bucket and returns a map of label to question-answer pair
 func LoadDatasetGCS(bucketName, objectName string) (map[string][]string, error) {
-    ctx := context.Background()
-    client, err := storage.NewClient(ctx)
+    data, err := ReadFileFromGCS(bucketName, objectName)
     if err != nil {
-        return nil, fmt.Errorf("failed to create storage client: %v", err)
+        return nil, fmt.Errorf("failed to read dataset file: %v", err)
     }
-    defer client.Close()
 
-    bucket := client.Bucket(bucketName)
-    object := bucket.Object(objectName)
-    r, err := object.NewReader(ctx)
-    if err != nil {
-        return nil, fmt.Errorf("failed to create reader for object: %v", err)
-    }
-    defer r.Close()
-
+    r := bytes.NewReader(data)
     reader := csv.NewReader(r)
     reader.Comma = '|' // Set the delimiter to pipe
     records, err := reader.ReadAll()
