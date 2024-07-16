36e787c95db42b1b596df6568cbacb96b95bd5c4
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-16 18:32:33 +0700

    add mongo connect

--- config/db.go
+++ config/db.go
@@ -1,17 +1,24 @@
 package config
 
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type DBInfo struct {
 	DBString string
 	DBName   string
 }
 
-// func MongoConnect(mconn DBInfo) (db *mongo.Database, err error)  {
-// 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return client.Database(mconn.DBName), nil
-// }
+func MongoConnect(mconn DBInfo) (db *mongo.Database, err error) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
+	if err != nil {
+		return nil, err
+	}
+	return client.Database(mconn.DBName), nil
+}
 
 // var MongoString string = GetEnv("MONGOSTRING")
 
