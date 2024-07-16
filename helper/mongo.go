7b27154daf0815bd34c550ea28c13adb61ff1ab1
erditona <erditonaushaadam@gmail.com>
2024-07-16 09:03:37 +0700

    update mongo.go

--- helper/mongo.go
+++ helper/mongo.go
@@ -32,6 +32,21 @@ import (
 // 	return doc, nil
 // }
 
+// func GetAllDocs[T any](db *mongo.Database, col string, filter bson.M) (docs T, err error) {
+// 	ctx := context.TODO()
+// 	collection := db.Collection(col)
+// 	cursor, err := collection.Find(ctx, filter)
+// 	if err != nil {
+// 		return 
+// 	}
+// 	defer cursor.Close(ctx)
+// 	err = cursor.All(context.TODO(), &docs)
+// 	if err != nil {
+// 		return 
+// 	}
+// 	return 
+// }
+
 
 
 
