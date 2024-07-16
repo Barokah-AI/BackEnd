3c930bb91a23f594bd054e7936b49ce01910a890
erditona <erditonaushaadam@gmail.com>
2024-07-16 09:00:05 +0700

    update db.go

--- config/db.go
+++ config/db.go
@@ -6,8 +6,17 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type DBInfo struct {
-	DBString string
-	DBName   string
-}
+// type DBInfo struct {
+// 	DBString string
+// 	DBName   string
+// }
+
+// func MongoConnect(mconn DBInfo) (db *mongo.Database, err error)  {
+// 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return client.Database(mconn.DBName), nil
+// }
+
 
