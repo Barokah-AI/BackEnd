060db1960d947412ac0c44e39d69d7b35d66a74d
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-16 18:38:41 +0700

    update get userfromemail

--- helper/mongo.go
+++ helper/mongo.go
@@ -2,7 +2,10 @@ package helper
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/Barokah-AI/BackEnd/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -15,18 +18,18 @@ func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive
 	return result.InsertedID.(primitive.ObjectID), nil
 }
 
-// func GetUserFromEmail(email string, db *mongo.Database) (doc model.User, err error) {
-// 	collection := db.Collection("users")
-// 	filter := bson.M{"email": email}
-// 	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
-// 	if err != nil {
-// 		if err == mongo.ErrNoDocuments {
-// 			return doc, fmt.Errorf("email tidak ditemukan")
-// 		}
-// 		return doc, fmt.Errorf("kesalahan server")
-// 	}
-// 	return doc, nil
-// }
+func GetUserFromEmail(email string, db *mongo.Database) (doc model.User, err error) {
+	collection := db.Collection("users")
+	filter := bson.M{"email": email}
+	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return doc, fmt.Errorf("email tidak ditemukan")
+		}
+		return doc, fmt.Errorf("kesalahan server")
+	}
+	return doc, nil
+}
 
 // func GetAllDocs[T any](db *mongo.Database, col string, filter bson.M) (docs T, err error) {
 // 	ctx := context.TODO()
