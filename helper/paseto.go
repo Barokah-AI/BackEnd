c90eb7b0a226d932322edb3e13226a37e5affb2e
erditona <erditonaushaadam@gmail.com>
2024-07-16 09:06:18 +0700

    update paseto.go

--- helper/paseto.go
+++ helper/paseto.go
@@ -8,3 +8,11 @@ import (
 	"aidanwoods.dev/go-paseto"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+// type Payload struct {
+// 	Id    primitive.ObjectID `json:"id"`
+// 	Email string             `json:"email"`
+// 	Exp   time.Time          `json:"exp"`
+// 	Iat   time.Time          `json:"iat"`
+// 	Nbf   time.Time          `json:"nbf"`
+// }
