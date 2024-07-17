1ba51659d50352d906fac29fb29961abcf3af3c8
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-17 18:13:13 +0700

    Fix error messages in callhf

--- helper/callhf.go
+++ helper/callhf.go
@@ -18,12 +18,12 @@ func CallHuggingFaceAPI(prompt string) (string, float64, error) {
     reqBody := model.HFRequest{Inputs: prompt}
     jsonReqBody, err := json.Marshal(reqBody)
     if err != nil {
-        return "", 0, fmt.Errorf("error marshalling request body: %v", err)
+        return "", 0, fmt.Errorf("error marshalling in request body: %v", err)
     }
 
     req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(jsonReqBody))
     if err != nil {
-        return "", 0, fmt.Errorf("error creating request: %v", err)
+        return "", 0, fmt.Errorf("error creating in request: %v", err)
     }
     req.Header.Set("Authorization", apiToken)
     req.Header.Set("Content-Type", "application/json")
@@ -31,23 +31,23 @@ func CallHuggingFaceAPI(prompt string) (string, float64, error) {
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
-        return "", 0, fmt.Errorf("error making request to Hugging Face API: %v", err)
+        return "", 0, fmt.Errorf("error making request in to Hugging Face API: %v", err)
     }
     defer resp.Body.Close()
 
     if resp.StatusCode != http.StatusOK {
         bodyBytes, _ := ioutil.ReadAll(resp.Body)
-        return "", 0, fmt.Errorf("unexpected status code from Hugging Face API: %d | Server HF Response: %s", resp.StatusCode, string(bodyBytes))
+        return "", 0, fmt.Errorf("unexpected status code from in Hugging Face API: %d | Server HF Response: %s", resp.StatusCode, string(bodyBytes))
     }
 
     var hfResponse []model.HFResponse
     err = json.NewDecoder(resp.Body).Decode(&hfResponse)
     if err != nil {
-        return "", 0, fmt.Errorf("error decoding response: %v", err)
+        return "", 0, fmt.Errorf("error decoding in response: %v", err)
     }
 
     if len(hfResponse) == 0 {
-        return "", 0, fmt.Errorf("empty response from Hugging Face API")
+        return "", 0, fmt.Errorf("empty response in from Hugging Face API")
     }
 
     bestResponse := hfResponse[0]
