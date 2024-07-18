8b4d86a9d50d1649bb69458775cad79ab0c2cbe1
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-18 23:04:21 +0700

    update response

--- controller/login.go
+++ controller/login.go
@@ -75,4 +75,16 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		},
 	}
 	helper.WriteJSON(respw, http.StatusOK, resp)
+
+	// response
+	// resp := map[string]any{
+	// 	"status":  "success",
+	// 	"message": "login berhasil",
+	// 	"token":   tokenstring,
+	// 	"data" : map[string]string{
+	// 		"email": existsDoc.Email,
+	// 		"namalengkap": existsDoc.NamaLengkap,
+	// 	},
+	// }
+	helper.WriteJSON(respw, http.StatusOK, resp)
 }
