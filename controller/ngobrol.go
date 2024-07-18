ea0a92e7bfb90f6eef0eea5470f5a1696e35c87b
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-19 00:51:45 +0700

    add func

--- controller/ngobrol.go
+++ controller/ngobrol.go
@@ -11,3 +11,12 @@ package controller
 // 	"github.com/Barokah-AI/BackEnd/helper"
 // 	"github.com/Barokah-AI/BackEnd/model"
 // )
+
+func Ngobrol(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
+    var chat model.AIRequest
+
+    err := json.NewDecoder(req.Body).Decode(&chat)
+    if err != nil {
+        helper.ErrorResponse(respw, req, http.StatusBadRequest, "Permintaan Tidak Valid", "error saat membaca isi permintaan: "+err.Error())
+        return
+    }
