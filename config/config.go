75e6e70de6418c06f17afb43a3f6847590cbc3fe
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-18 23:44:20 +0700

    update function getEnv

--- config/config.go
+++ config/config.go
@@ -4,13 +4,7 @@ import
 // "github.com/joho/godotenv"
 "os"
 
-// func GetEnv(envName string) string {
-// 	// envFile, _ := godotenv.Read("../.env")
-// 	// return envFile[envName]
-// 	return os.Getenv(envName)
-// }
-
-func Getenv(envName string) string {
+func GetEnv(envName string) string {
 	// envFile, _ := godotenv.Read("../.env")
 	// return envFile[envName]
 	return os.Getenv(envName)
