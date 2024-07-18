7f57f8b0b4e0ec83d20b65a210be9be7ab309f71
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-18 22:28:12 +0700

    load datasetGCS

--- helper/load-dataset-gcs.go
+++ helper/load-dataset-gcs.go
@@ -34,3 +34,30 @@ func LoadDatasetGCS(bucketName, objectName string) (map[string][]string, error) 
     }
     return labelToQA, nil
 }
+
+// LoadDataset loads the dataset from GCS bucket and returns a map of label to question-answer pair
+func LoaddatasetGCS(bucketName, objectName string) (map[string][]string, error) {
+    data, err := ReadFileFromGCS(bucketName, objectName)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read dataset file: %v", err)
+    }
+
+    r := bytes.NewReader(data)
+    reader := csv.NewReader(r)
+    reader.Comma = '|' // Set the delimiter to pipe
+    records, err := reader.ReadAll()
+    if err != nil {
+        return nil, fmt.Errorf("failed to read dataset file: %v", err)
+    }
+
+    labelToQA := make(map[string][]string)
+    for i, record := range records {
+        if len(record) != 2 {
+            log.Printf("Skipping invalid record at line %d: %v\n", i+1, record)
+            continue
+        }
+        label := "LABEL_" + strconv.Itoa(i)
+        labelToQA[label] = record
+    }
+    return labelToQA, nil
+}
