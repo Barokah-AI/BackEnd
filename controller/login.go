e3180068eac6608d79f1d1e10b19c4a91e8659b9
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-19 00:36:39 +0700

    validate

--- controller/login.go
+++ controller/login.go
@@ -23,3 +23,9 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body " + err.Error())
 		return
 	}
+
+	// check if email and password is empty
+	if user.Email == "" || user.Password == "" {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		return
+	}
