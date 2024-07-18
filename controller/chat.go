f87ab02c9e4d12afe834f97cb1348290922eae92
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-19 00:28:17 +0700

    Add Hugging Face API URL and token to Chat function

--- controller/chat.go
+++ controller/chat.go
@@ -14,7 +14,7 @@ package controller
 
 func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
     var chat model.AIRequest
-	
+
 	err := json.NewDecoder(req.Body).Decode(&chat)
     if err != nil {
         helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
@@ -27,3 +27,9 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
     }
 
     client := resty.New()
+
+	  // Hugging Face API URL dan token
+	  apiUrl := config.GetEnv("HUGGINGFACE_API_URL")
+	  apiToken := "Bearer " + config.GetEnv("HUGGINGFACE_API_KEY")
+  
+	
