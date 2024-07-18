ad537934c593b897956a9dbab4171563fd314d31
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-18 12:11:49 +0700

    delete

--- config/config.go
+++ config/config.go
@@ -1,11 +1,1 @@
 package config
-
-import
-// "github.com/joho/godotenv"
-"os"
-
-func GetEnv(envName string) string {
-	// envFile, _ := godotenv.Read("../.env")
-	// return envFile[envName]
-	return os.Getenv(envName)
-}
