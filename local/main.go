adf83c774b6c9e57574419258de30c0670af37f0
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-18 18:37:22 +0700

    Refactor main.go to enable server startup and remove commented out code

--- local/main.go
+++ local/main.go
@@ -7,12 +7,12 @@ import (
 	routes "github.com/Barokah-AI/BackEnd/local/url"
 )
 
-// func main() {
-// 	http.HandleFunc("/", routes.URL)
-// 	port := ":8080"
-// 	log.Printf("Starting server on port %s", port + " or click this link http://localhost:8080")
-// 	err := http.ListenAndServe(port, nil)
-// 	if err != nil {
-// 		log.Fatalf("Error starting server: %v", err)
-// 	}
-// }
+func main() {
+	http.HandleFunc("/", routes.URL)
+	port := ":8080"
+	log.Printf("Starting server on port %s", port + " or click this link http://localhost:8080")
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
