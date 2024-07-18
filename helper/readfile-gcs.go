1bdead815f7f4f727ac49b86a4bfa29060b6451e
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-18 23:08:23 +0700

    remove file dari GCS

--- helper/readfile-gcs.go
+++ helper/readfile-gcs.go
@@ -33,28 +33,3 @@ func ReadFileFromGCS(bucketName, fileName string) ([]byte, error) {
 
 	return data, nil
 }
-
-func ReadfileFromGCS(bucketName, fileName string) ([]byte, error) {
-	ctx := context.Background()
-
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create client: %v", err)
-	}
-	defer client.Close()
-
-	bucket := client.Bucket(bucketName)
-	obj := bucket.Object(fileName)
-	r, err := obj.NewReader(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create reader: %v", err)
-	}
-	defer r.Close()
-
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read data: %v", err)
-	}
-
-	return data, nil
-}
