4cc83920d67048108141990930aedab06046c67d
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-18 23:09:04 +0700

    update function

--- local/main.go
+++ local/main.go
@@ -7,7 +7,17 @@ import (
 	routes "github.com/Barokah-AI/BackEnd/local/url"
 )
 
-func main() {
+// func main() {
+// 	http.HandleFunc("/", routes.URL)
+// 	port := ":8080"
+// 	log.Printf("Starting server on port %s", port + " or click this link http://localhost:8080")
+// 	err := http.ListenAndServe(port, nil)
+// 	if err != nil {
+// 		log.Fatalf("Error starting server: %v", err)
+// 	}
+// }
+
+func main1() {
 	http.HandleFunc("/", routes.URL)
 	port := ":8080"
 	log.Printf("Starting server on port %s", port + " or click this link http://localhost:8080")
