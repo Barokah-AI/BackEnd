f80c01f4350328b4178f835bde4ea548368005ef
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-19 00:36:07 +0700

    add login

--- controller/login.go
+++ controller/login.go
@@ -12,3 +12,8 @@ package controller
 // 	"github.com/Barokah-AI/BackEnd/helper"
 // 	"github.com/Barokah-AI/BackEnd/model"
 // )
+
+// user
+func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {
+	var user model.User
+	err := json.NewDecoder(req.Body).Decode(&user)
