44b37e610b7dd0a09fdcc496d1467c12fbfcccaa
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-18 12:16:52 +0700

    create origins

--- config/cors.go
+++ config/cors.go
@@ -1,1 +1,15 @@
 package config
+
+// import (
+// 	"net/http"
+// )
+
+// Daftar origins yang diizinkan
+var Origins = []string{
+	"http://localhost:8080",
+	"http://localhost:5173",
+	"http://127.0.0.1:5500",
+	"http://127.0.0.1:5501",
+	"http://127.0.0.1:5503",
+	"https://barokah-ai.vercel.app",
+}
