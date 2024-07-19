d603b9effea862933b156405a5362148d1201b03
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-19 12:09:36 +0700

    Refactor dataset loading logic

--- helper/load-dataset-gcs.go
+++ helper/load-dataset-gcs.go
@@ -10,18 +10,18 @@ import (
 
 // LoadDataset loads the dataset from GCS bucket and returns a map of label to question-answer pair
 func LoadDatasetGCS(bucketName, objectName string) (map[string][]string, error) {
-    // data, err := ReadFileFromGCS(bucketName, objectName)
-    // if err != nil {
-    //     return nil, fmt.Errorf("failed to read dataset file: %v", err)
-    // }
+    data, err := ReadFileFromGCS(bucketName, objectName)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read dataset file: %v", err)
+    }
 
-    // r := bytes.NewReader(data)
-    // reader := csv.NewReader(r)
-    // reader.Comma = '|' // Set the delimiter to pipe
-    // records, err := reader.ReadAll()
-    // if err != nil {
-    //     return nil, fmt.Errorf("failed to read dataset file: %v", err)
-    // }
+    r := bytes.NewReader(data)
+    reader := csv.NewReader(r)
+    reader.Comma = '|' // Set the delimiter to pipe
+    records, err := reader.ReadAll()
+    if err != nil {
+        return nil, fmt.Errorf("failed to read dataset file: %v", err)
+    }
 
     labelToQA := make(map[string][]string)
     for i, record := range records {
