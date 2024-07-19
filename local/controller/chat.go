e97fce7f18eb6e61226f81b22d3e80990360c00f
stefanymelda <juwitastefany13@gmail.com>
2024-07-19 10:24:46 +0700

    controller API request code

--- local/controller/chat.go
+++ local/controller/chat.go
@@ -18,18 +18,18 @@ func callHuggingFaceAPI(prompt string) (string, float64, error) {
 	apiUrl := config.GetEnv("HUGGINGFACE_API_URL")
 	apiToken := "Bearer " + config.GetEnv("HUGGINGFACE_API_KEY")
 
-	// reqBody := model.HFRequest{Inputs: prompt}
-	// jsonReqBody, err := json.Marshal(reqBody)
-	// if err != nil {
-	// 	return "", 0, fmt.Errorf("error marshalling request body: %v", err)
-	// }
+	reqBody := model.HFRequest{Inputs: prompt}
+	jsonReqBody, err := json.Marshal(reqBody)
+	if err != nil {
+		return "", 0, fmt.Errorf("error marshalling request body: %v", err)
+	}
 
-	// req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(jsonReqBody))
-	// if err != nil {
-	// 	return "", 0, fmt.Errorf("error creating request: %v", err)
-	// }
-	// req.Header.Set("Authorization", apiToken)
-	// req.Header.Set("Content-Type", "application/json")
+	req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(jsonReqBody))
+	if err != nil {
+		return "", 0, fmt.Errorf("error creating request: %v", err)
+	}
+	req.Header.Set("Authorization", apiToken)
+	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
