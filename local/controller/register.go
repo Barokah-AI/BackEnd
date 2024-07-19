6b26c7894470f5bf0d38e8df2c34ee3e820ef4cf
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-19 12:15:12 +0700

    update user sign up logic and error handling

--- local/controller/register.go
+++ local/controller/register.go
@@ -60,25 +60,25 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 
 	hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 
-	// userData := bson.M{
-	// 	"namalengkap": user.NamaLengkap,
-	// 	"email":       user.Email,
-	// 	"password":    hex.EncodeToString(hashedPassword),
-	// 	"salt":        hex.EncodeToString(salt),
-	// }
+	userData := bson.M{
+		"namalengkap": user.NamaLengkap,
+		"email":       user.Email,
+		"password":    hex.EncodeToString(hashedPassword),
+		"salt":        hex.EncodeToString(salt),
+	}
 
-	// insertedID, err := helper.InsertOneDoc(db, col, userData)
-	// if err != nil {
-	// 	helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
-	// 	return
-	// }
+	insertedID, err := helper.InsertOneDoc(db, col, userData)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
+		return
+	}
 
-	// resp := map[string]any{
-	// 	"message":    "berhasil mendaftar",
-	// 	"insertedID": insertedID,
-	// 	"data": map[string]string{
-	// 		"email": user.Email,
-	// 	},
-	// }
-	// helper.WriteJSON(respw, http.StatusCreated, resp)
+	resp := map[string]any{
+		"message":    "berhasil mendaftar",
+		"insertedID": insertedID,
+		"data": map[string]string{
+			"email": user.Email,
+		},
+	}
+	helper.WriteJSON(respw, http.StatusCreated, resp)
 }
