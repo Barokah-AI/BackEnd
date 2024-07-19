1cf36dd80fa29c48bf8bb21584ab116f4b97e072
stefanymelda <juwitastefany13@gmail.com>
2024-07-19 10:29:34 +0700

    user sign up endpoint

--- local/controller/register.go
+++ local/controller/register.go
@@ -19,16 +19,16 @@ import (
 func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
 	var user model.User
 
-	err := json.NewDecoder(req.Body).Decode(&user)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
+	// err := json.NewDecoder(req.Body).Decode(&user)
+	// if err != nil {
+	// 	helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+	// 	return
+	// }
 
-	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-		return
-	}
+	// if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
+	// 	helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+	// 	return
+	// }
 
 	if err := checkmail.ValidateFormat(user.Email); err != nil {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
