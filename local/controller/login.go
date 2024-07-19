1719df203e9aff81791ff510e8d838024088577b
stefanymelda <juwitastefany13@gmail.com>
2024-07-19 10:39:47 +0700

    return JSON response

--- local/controller/login.go
+++ local/controller/login.go
@@ -65,14 +65,14 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	}
 
 	// response
-	// resp := map[string]any{
-	// 	"status":  "success",
-	// 	"message": "login berhasil",
-	// 	"token":   tokenstring,
-	// 	"data" : map[string]string{
-	// 		"email": existsDoc.Email,
-	// 		"namalengkap": existsDoc.NamaLengkap,
-	// 	},
-	// }
-	// helper.WriteJSON(respw, http.StatusOK, resp)
+	resp := map[string]any{
+		"status":  "success",
+		"message": "login berhasil",
+		"token":   tokenstring,
+		"data" : map[string]string{
+			"email": existsDoc.Email,
+			"namalengkap": existsDoc.NamaLengkap,
+		},
+	}
+	helper.WriteJSON(respw, http.StatusOK, resp)
 }
