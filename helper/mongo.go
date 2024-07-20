61fdcee7c04784b0789fb991131b2c31abf15240
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 18:26:08 +0700

    delete import

--- helper/mongo.go
+++ helper/mongo.go
@@ -19,8 +19,6 @@ import (
 // 	"github.com/Barokah-AI/BackEnd/model"
 // 	"go.mongodb.org/mongo-driver/bson"
 // 	"go.mongodb.org/mongo-driver/bson/primitive"
-// 	"go.mongodb.org/mongo-driver/mongo"
-// )
 
 func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive.ObjectID, err error) {
 	result, err := db.Collection(col).InsertOne(context.Background(), doc)
