daa7666896d66a586e03d32a873cc74e11ee202f
stefanymelda <juwitastefany13@gmail.com>
2024-07-20 14:30:25 +0700

    update config file

--- config/config.go
+++ config/config.go
@@ -4,8 +4,8 @@ import
 "github.com/joho/godotenv"
 "os"
 
-func GetEnv(envName string) string {
-	envFile, _ := godotenv.Read("../.env")
-	return envFile[envName]
-	return os.Getenv(envName)
-}
+// func GetEnv(envName string) string {
+// 	envFile, _ := godotenv.Read("../.env")
+// 	return envFile[envName]
+// 	return os.Getenv(envName)
+// }
