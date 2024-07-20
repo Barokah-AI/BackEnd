aadb94160367c375f1b2c01f52f280af6dc5a252
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 18:26:25 +0700

    Remove unused imports and refactor InsertOneDoc function in mongo.go

--- helper/mongo.go
+++ helper/mongo.go
@@ -17,8 +17,6 @@ import (
 // 	"fmt"
 
 // 	"github.com/Barokah-AI/BackEnd/model"
-// 	"go.mongodb.org/mongo-driver/bson"
-// 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive.ObjectID, err error) {
 	result, err := db.Collection(col).InsertOne(context.Background(), doc)
