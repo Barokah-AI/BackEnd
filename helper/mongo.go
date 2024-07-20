345faec737af9f7b29164096970d364df330dc57
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 18:21:35 +0700

    insertonedoc

--- helper/mongo.go
+++ helper/mongo.go
@@ -1,5 +1,12 @@
 package helper
 
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 // import (
 // 	"context"
 // 	"errors"
@@ -10,3 +17,11 @@ package helper
 // 	"go.mongodb.org/mongo-driver/bson/primitive"
 // 	"go.mongodb.org/mongo-driver/mongo"
 // )
+
+func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive.ObjectID, err error) {
+	result, err := db.Collection(col).InsertOne(context.Background(), doc)
+	if err != nil {
+		return
+	}
+	return result.InsertedID.(primitive.ObjectID), nil
+}
