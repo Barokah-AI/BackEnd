5ca3e380964c04cdc6ee651b467e9619b5f7b28d
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 18:22:24 +0700

    eror condition

--- helper/mongo.go
+++ helper/mongo.go
@@ -2,7 +2,10 @@ package helper
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/Barokah-AI/BackEnd/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -30,3 +33,11 @@ func GetUserFromEmail(email string, db *mongo.Database) (doc model.User, err err
 	collection := db.Collection("users")
 	filter := bson.M{"email": email}
 	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return doc, fmt.Errorf("email tidak ditemukan")
+		}
+		return doc, fmt.Errorf("kesalahan server")
+	}
+	return doc, nil
+}
