f9dac544b5f91362ac51e40308364d6d6e0b8e9d
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-22 15:23:34 +0700

    refactor: Update Paseto helper to use latest version of go-paseto library

--- helper/paseto.go
+++ helper/paseto.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"time"
 
+	"aidanwoods.dev/go-paseto"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -31,3 +32,5 @@ func Encode(id primitive.ObjectID, email, privateKey string) (string, error) {
 	token.Set("id", id)
 	token.SetString("email", email)
 	secretKey, err := paseto.NewV4AsymmetricSecretKeyFromHex(privateKey)
+	return token.V4Sign(secretKey, nil), err
+}
