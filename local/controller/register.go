cf1d66b38a97648f48e7e002b98811be48409ff8
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-23 10:11:09 +0700

    user sign up  and error handling

--- local/controller/register.go
+++ local/controller/register.go
@@ -51,27 +51,27 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		return
 	}
 
-	// salt := make([]byte, 16)
-	// _, err = rand.Read(salt)
-	// if err != nil {
-	// 	helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
-	// 	return
-	// }
+	salt := make([]byte, 16)
+	_, err = rand.Read(salt)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
+		return
+	}
 
-	// hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
+	hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 
-	// userData := bson.M{
-	// 	"namalengkap": user.NamaLengkap,
-	// 	"email":       user.Email,
-	// 	"password":    hex.EncodeToString(hashedPassword),
-	// 	"salt":        hex.EncodeToString(salt),
-	// }
+	userData := bson.M{
+		"namalengkap": user.NamaLengkap,
+		"email":       user.Email,
+		"password":    hex.EncodeToString(hashedPassword),
+		"salt":        hex.EncodeToString(salt),
+	}
 
-	// insertedID, err := helper.InsertOneDoc(db, col, userData)
-	// if err != nil {
-	// 	helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
-	// 	return
-	// }
+	insertedID, err := helper.InsertOneDoc(db, col, userData)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
+		return
+	}
 
 	resp := map[string]any{
 		"message":    "berhasil mendaftar",
