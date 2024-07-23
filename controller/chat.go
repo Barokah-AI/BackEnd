bf553140486e712b31b3b28c0d864c2a9deef0e6
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-23 21:33:48 +0700

    update chat

--- controller/chat.go
+++ controller/chat.go
@@ -119,4 +119,3 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
         helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : response")
     }
 }
-
