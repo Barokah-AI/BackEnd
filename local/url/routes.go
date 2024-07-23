116d3b9c3b59d64bbc6858040d8f7e51ccb76a7c
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-23 10:13:05 +0700

    URL routing logicand readability

--- local/url/routes.go
+++ local/url/routes.go
@@ -8,24 +8,24 @@ import (
 	"github.com/Barokah-AI/BackEnd/local/controller"
 )
 
-func URL(w http.ResponseWriter, r *http.Request) {
-	if config.SetAccessControlHeaders(w, r) {
-		return // If it's a preflight request, return early.
-	}
+// func URL(w http.ResponseWriter, r *http.Request) {
+// 	if config.SetAccessControlHeaders(w, r) {
+// 		return // If it's a preflight request, return early.
+// 	}
 
-	var method, path string = r.Method, r.URL.Path
-	switch {
-	case method == "GET" && path == "/":
-		Home(w, r)
-	case method == "POST" && path == "/chat":
-		controller.Chat(w, r, config.GetEnv("HUGGINGFACE_API_KEY"))
-	case method == "POST" && path == "/signup":
-		controller.SignUp(config.Mongoconn, "users", w, r)
-	case method == "POST" && path == "/login":
-		controller.LogIn(config.Mongoconn, w, r, config.GetEnv("PASETOPRIVATEKEY"))
-	default:
-		helper.ErrorResponse(w, r, http.StatusNotFound, "Not Found", "The requested resource was not found")
-	}
+// 	var method, path string = r.Method, r.URL.Path
+// 	switch {
+// 	case method == "GET" && path == "/":
+// 		Home(w, r)
+// 	case method == "POST" && path == "/chat":
+// 		controller.Chat(w, r, config.GetEnv("HUGGINGFACE_API_KEY"))
+// 	case method == "POST" && path == "/signup":
+// 		controller.SignUp(config.Mongoconn, "users", w, r)
+// 	case method == "POST" && path == "/login":
+// 		controller.LogIn(config.Mongoconn, w, r, config.GetEnv("PASETOPRIVATEKEY"))
+// 	default:
+// 		helper.ErrorResponse(w, r, http.StatusNotFound, "Not Found", "The requested resource was not found")
+// 	}
 }
 
 func Home(respw http.ResponseWriter, req *http.Request) {
