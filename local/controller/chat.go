55c6f034f076b5870a88a0ed8c4e9393c8f41f4a
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-25 19:10:37 +0700

    refactor: Update HTTP request in callHuggingFaceAPI function

--- local/controller/chat.go
+++ local/controller/chat.go
@@ -24,3 +24,10 @@ func callHuggingFaceAPI(prompt string) (string, float64, error) {
 		return "", 0, fmt.Errorf("error marshalling request body: %v", err)
 	}
 
+	req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(jsonReqBody))
+	if err != nil {
+		return "", 0, fmt.Errorf("error creating request: %v", err)
+	}
+	req.Header.Set("Authorization", apiToken)
+	req.Header.Set("Content-Type", "application/json")
+
