4f6eccea51819acf93fbbc90691df072be75f9bd
dani ferdinan <daniferdinandall@gmail.com>
2024-07-25 18:55:31 +0700

    update chat.go

--- controller/chat.go
+++ controller/chat.go
@@ -91,10 +91,10 @@ func Chat(respwd http.ResponseWriter, request *http.Request, tokenmodel string) 
 
 	if best_label != "" {
 		// Load the dataset from GCS
-		bucketName := config.GetEnv("GCS_BUCKET_NAME")
+		bucket_name := config.GetEnv("GCS_BUCKET_NAME")
 		objectName := config.GetEnv("GCS_DATASET_FILE")
 
-		label_to_qa, err := helper.LoadDatasetGCS(bucketName, objectName)
+		label_to_qa, err := helper.LoadDatasetGCS(bucket_name, objectName)
 		if err != nil {
 			helper.ErrorResponse(respwd, request, http.StatusInternalServerError, "Internal Server Error", "server error: could not load dataset: "+err.Error())
 			return
