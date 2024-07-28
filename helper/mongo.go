ab2c6d23cbf9a16c8d937f61b7f8a58b5f238cb5
AdeCandra12 <ade.can321@gmail.com>
2024-07-29 00:40:18 +0700

    feat: Add helper and paseto packages for MongoDB integration

---
+++ helper/mongo.go
@@ -0,0 +1,12 @@
+package helper
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/Barokah-AI/BackEnd/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
---
+++ helper/paseto.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"aidanwoods.dev/go-paseto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Payload struct {
+	Id    primitive.ObjectID `json:"id"`
+	Email string             `json:"email"`
+	Exp   time.Time          `json:"exp"`
+	Iat   time.Time          `json:"iat"`
+	Nbf   time.Time          `json:"nbf"`
+}
