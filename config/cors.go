ab53ad6223c057cd33f78f2311557132e3404857
AdeCandra12 <ade.can321@gmail.com>
2024-07-29 00:20:56 +0700

    refactor: Remove unused origins and update CORS configuration

--- config/cors.go
+++ config/cors.go
@@ -3,3 +3,13 @@ package config
 import (
 	"net/http"
 )
+
+// Daftar origins yang diizinkan
+var Origins = []string{
+	"http://localhost:8080",
+	"http://localhost:5173",
+	"http://127.0.0.1:5500",
+	"http://127.0.0.1:5501",
+	"http://127.0.0.1:5503",
+	"https://barokah-ai.vercel.app",
+}
