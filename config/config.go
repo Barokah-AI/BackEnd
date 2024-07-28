569ed24ec6830d67037d8d4a9d58ebd9d4b57279
AdeCandra12 <ade.can321@gmail.com>
2024-07-29 00:19:17 +0700

    Remove unused GetEnv function and update functionality

--- config/config.go
+++ config/config.go
@@ -4,4 +4,8 @@ import
 // "github.com/joho/godotenv"
 "os"
 
-func GetEnv(envName string) string {}
+func GetEnv(envName string) string {
+	// envFile, _ := godotenv.Read("../.env")
+	// return envFile[envName]
+	return os.Getenv(envName)
+}
