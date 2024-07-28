6870628c7f7665f96fa357e0b0fb31e4a133989d
AdeCandra12 <ade.can321@gmail.com>
2024-07-29 00:32:33 +0700

    refactor: Add login controller

---
+++ controller/login.go
@@ -0,0 +1,14 @@
+package controller
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"net/http"
+
+	"github.com/badoux/checkmail"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/argon2"
+
+	"github.com/Barokah-AI/BackEnd/helper"
+	"github.com/Barokah-AI/BackEnd/model"
+)
