ea9b9fcc38a0e6c631d04c0bfa6626036a9f301c
AdeCandra12 <ade.can321@gmail.com>
2024-07-30 14:18:03 +0700

    refactor: Update LoadDatasetLocal function to load dataset from local file

--- helper/load-dataset-local.go
+++ helper/load-dataset-local.go
@@ -7,3 +7,6 @@ import (
 	"os"
 	"strconv"
 )
+
+// LoadDataset loads the dataset from the given CSV file and returns a map of label to question-answer pair
+func LoadDatasetLocal(filePath string) (map[string][]string, error) {}
