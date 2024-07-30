dd013d152b54bf7505469754fc11d8196c64be46
AdeCandra12 <ade.can321@gmail.com>
2024-07-30 14:49:19 +0700

    Add lazy loading for images in WebHook function

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package barokahai
+
+import (
+	routes "github.com/Barokah-AI/BackEnd/url"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("WebHook", routes.URL)
+}
