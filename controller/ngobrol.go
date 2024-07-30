2f67c12ab765f242351932f17ce57ff8631004d4
AdeCandra12 <ade.can321@gmail.com>
2024-07-30 14:38:07 +0700

    Add input validation and error handling to Ngobrol function

--- controller/ngobrol.go
+++ controller/ngobrol.go
@@ -12,4 +12,18 @@ import (
 	"github.com/Barokah-AI/BackEnd/model"
 )
 
-func Ngobrol(respwrt http.ResponseWriter, req *http.Request, tokenmodel string) {}
+func Ngobrol(respwrt http.ResponseWriter, req *http.Request, tokenmodel string) {
+	var chat model.AIRequest
+
+	err := json.NewDecoder(req.Body).Decode(&chat)
+	if err != nil {
+		helper.ErrorResponse(respwrt, req, http.StatusBadRequest, "Permintaan Tidak Valid", "error saat membaca isi permintaan: "+err.Error())
+		return
+	}
+
+	if chat.Prompt == "" {
+		helper.ErrorResponse(respwrt, req, http.StatusBadRequest, "Permintaan Tidak Valid", "masukin pertanyaan dulu ya kakak 🤗")
+		return
+	}
+
+}
