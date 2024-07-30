5228a559fa7d9bb4d90fe674a031ec0d13a24ddd
AdeCandra12 <ade.can321@gmail.com>
2024-07-30 14:18:31 +0700

    refactor: Add Encode function to generate PASETO token with id, email, and privateKey

--- helper/paseto.go
+++ helper/paseto.go
@@ -16,3 +16,14 @@ type Payload struct {
 	Iat   time.Time          `json:"iat"`
 	Nbf   time.Time          `json:"nbf"`
 }
+
+func Encode(id primitive.ObjectID, email, privateKey string) (string, error) {
+	token := paseto.NewToken()
+	token.SetIssuedAt(time.Now())
+	token.SetNotBefore(time.Now())
+	token.SetExpiration(time.Now().Add(2 * time.Hour))
+	token.Set("id", id)
+	token.SetString("email", email)
+	secretKey, err := paseto.NewV4AsymmetricSecretKeyFromHex(privateKey)
+	return token.V4Sign(secretKey, nil), err
+}
