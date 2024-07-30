ce4d275eaf2cd41275d6a6d7a7925fa84ae72b3c
AdeCandra12 <ade.can321@gmail.com>
2024-07-30 14:45:59 +0700

    update login

--- controller/login.go
+++ controller/login.go
@@ -22,4 +22,55 @@ func LogIn(db *mongo.Database, respwrt http.ResponseWriter, request *http.Reques
 		helper.ErrorResponse(respwrt, request, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
 		return
 	}
+	// check if email and password is empty
+	if user.Email == "" || user.Password == "" {
+		helper.ErrorResponse(respwrt, request, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		return
+	}
+
+	// check if email is valid
+	if err = checkmail.ValidateFormat(user.Email); err != nil {
+		helper.ErrorResponse(respwrt, request, http.StatusBadRequest, "Bad Request", "email tidak valid")
+		return
+	}
+
+	// check if email exists
+	exists_doc, err := helper.GetUserFromEmail(user.Email, db)
+	if err != nil {
+		helper.ErrorResponse(respwrt, request, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email "+err.Error())
+		return
+	}
+
+	// check if password is correct
+	salt, err := hex.DecodeString(exists_doc.Salt)
+	if err != nil {
+		helper.ErrorResponse(respwrt, request, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
+		return
+	}
+
+	// compare password
+	hashpwd := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
+	if hex.EncodeToString(hashpwd) != exists_doc.Password {
+		helper.ErrorResponse(respwrt, request, http.StatusUnauthorized, "Unauthorized", "password salah")
+		return
+	}
+
+	// generate token
+	token_string, err := helper.Encode(user.ID, user.Email, privatekey)
+	if err != nil {
+		helper.ErrorResponse(respwrt, request, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : token")
+		return
+	}
+
+	// response
+	response := map[string]any{
+		"status":  "success",
+		"message": "login berhasil",
+		"token":   token_string,
+		"data": map[string]string{
+			"email":       exists_doc.Email,
+			"namalengkap": exists_doc.NamaLengkap,
+		},
+	}
+	helper.WriteJSON(respwrt, http.StatusOK, response)
 }
